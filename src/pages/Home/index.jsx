991c809be0f262951a66d089374d5cb5a87f378e
Patricia <englishwithpatricia007@gmail.com>
2025-04-28 14:47:40 -0300

    ajustes do frontend

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,14 +1,40 @@
 
 import './style.css'
+import Trash from '../../assets/trash.svg'
 
 function Home() {
 
+  const users = [
+    { id: '23456789', name: 'Lucas', email: 'lucas@gmail.com' },
+    { id: '98765432', name: 'Rodolfo', email: 'rodolfo@gmail.com' },
+    { id: '333333', name: 'Paulo', email: 'paulo@gmail.com' }
+  ]
+
   return (
-    
-      <div>
-        <h1>Olá React</h1>        
-      </div>
-    
+
+    <div className='container'>
+      <form>
+        <h1> Cadastro de Usuários </h1>
+        <input placeholder="Nome" name="name" type="text" />
+        <input placeholder="E-mail" email="email" type="email" />
+        <button type="button">Cadastrar</button>
+      </form>
+
+
+      {users.map(user => (
+        <div key={user.id} className='cards'>
+          <div>
+            <p>Nome: <span>{user.name}</span></p>
+            <p>Email: <span>{user.email}</span></p>
+          </div>
+          <button>
+            <img src={Trash} alt="trash" />
+          </button>
+        </div>
+      ))}
+    </div>
+
+
   )
 }
 
